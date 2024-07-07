a0a11f04e66d32d5a0da5e5d2a821e86742c69f3
Vania <aktives685@gmail.com>
2024-07-07 20:06:19 +0300

    finish students page

---
+++ js/info_card.js
@@ -0,0 +1,56 @@
+// Harry
+const Harry = document.querySelector(".harry-info");
+const HarryBtnOpen = document.querySelector(".harry-btn-open");
+const HarryBtnClose = document.querySelector(".harry-btn-close");
+const toggleHarry = () => Harry.classList.toggle("is-hidden");
+HarryBtnOpen.addEventListener("click", toggleHarry);
+HarryBtnClose.addEventListener("click", toggleHarry);
+// Hermione
+const Hermione = document.querySelector(".hermione-info");
+const HermioneBtnOpen = document.querySelector(".hermione-btn-open");
+const HermioneBtnClose = document.querySelector(".hermione-btn-close");
+const toggleHermione = () => Hermione.classList.toggle("is-hidden");
+HermioneBtnOpen.addEventListener("click", toggleHermione);
+HermioneBtnClose.addEventListener("click", toggleHermione);
+// Ron
+const Ron = document.querySelector(".ron-info");
+const RonBtnOpen = document.querySelector(".ron-btn-open");
+const RonBtnClose = document.querySelector(".ron-btn-close");
+const toggleRon = () => Ron.classList.toggle("is-hidden");
+RonBtnOpen.addEventListener("click", toggleRon);
+RonBtnClose.addEventListener("click", toggleRon);
+// Draco
+const Draco = document.querySelector(".draco-info");
+const DracoBtnOpen = document.querySelector(".draco-btn-open");
+const DracoBtnClose = document.querySelector(".draco-btn-close");
+const toggleDraco = () => Draco.classList.toggle("is-hidden");
+DracoBtnOpen.addEventListener("click", toggleDraco);
+DracoBtnClose.addEventListener("click", toggleDraco);
+// Cedric
+const Cedric = document.querySelector(".cedric-info");
+const CedricBtnOpen = document.querySelector(".cedric-btn-open");
+const CedricBtnClose = document.querySelector(".cedric-btn-close");
+const toggleCedric = () => Cedric.classList.toggle("is-hidden");
+CedricBtnOpen.addEventListener("click", toggleCedric);
+CedricBtnClose.addEventListener("click", toggleCedric);
+// Cho
+const Cho = document.querySelector(".cho-info");
+const ChoBtnOpen = document.querySelector(".cho-btn-open");
+const ChoBtnClose = document.querySelector(".cho-btn-close");
+const toggleCho = () => Cho.classList.toggle("is-hidden");
+ChoBtnOpen.addEventListener("click", toggleCho);
+ChoBtnClose.addEventListener("click", toggleCho);
+// Neville
+const Neville = document.querySelector(".neville-info");
+const NevilleBtnOpen = document.querySelector(".neville-btn-open");
+const NevilleBtnClose = document.querySelector(".neville-btn-close");
+const toggleNeville = () => Neville.classList.toggle("is-hidden");
+NevilleBtnOpen.addEventListener("click", toggleNeville);
+NevilleBtnClose.addEventListener("click", toggleNeville);
+// Luna
+const Luna = document.querySelector(".luna-info");
+const LunaBtnOpen = document.querySelector(".luna-btn-open");
+const LunaBtnClose = document.querySelector(".luna-btn-close");
+const toggleLuna = () => Luna.classList.toggle("is-hidden");
+LunaBtnOpen.addEventListener("click", toggleLuna);
+LunaBtnClose.addEventListener("click", toggleLuna);
